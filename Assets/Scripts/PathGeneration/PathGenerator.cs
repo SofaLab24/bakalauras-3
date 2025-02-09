d9084b5f858e9a8b9eafc020be41e6a5827fb7ce
SofaLab24 <vincas.genys@gmail.com>
2025-02-09 13:19:28 +0200

    small update

--- Assets/Scripts/PathGeneration/PathGenerator.cs
+++ Assets/Scripts/PathGeneration/PathGenerator.cs
@@ -8,6 +8,24 @@ using UnityEngine.Tilemaps;
 
 public class PathGenerator : MonoBehaviour
 {
+    private struct Path
+    {
+        public Dictionary<Vector2Int, PathTile> pathTiles;
+        public List<GameObject> enemyWalkPoints;
+        public bool hasEnd;
+        public Vector2Int lastFilledTile;
+        public Vector2Int targetTile;
+
+        public Path(Dictionary<Vector2Int, PathTile> pathTiles, List<GameObject> enemyWalkPoints, Vector2Int lastFilledTile, Vector2Int targetTile)
+        {
+            this.pathTiles = pathTiles;
+            this.enemyWalkPoints = enemyWalkPoints;
+            this.lastFilledTile = lastFilledTile;
+            this.targetTile = targetTile;
+            hasEnd = false;
+        }
+    }
+
     [SerializeField]
     Tilemap tilemap;
     [SerializeField]
@@ -19,20 +37,19 @@ public class PathGenerator : MonoBehaviour
 
     public int tileSize = 3;
 
-    private Dictionary<(int, int), PathTile> pathTiles;
+    private Dictionary<Vector2Int, PathTile> allTiles;
+    private List<Path> paths;
 
 
     public void Start()
     {
-        pathTiles = new Dictionary<(int, int), PathTile>();
+        allTiles = new Dictionary<Vector2Int, PathTile>();
         for (int i = 0; i < 4; i++)
         {
-            pathTiles[(0, i)] = new PathTile(new Vector2Int(0, i), tileSize);
-            pathTiles[(0, i)].GeneratePath(Vector2Int.down, Vector2Int.up);
-            PaintPathTile(pathTiles[(0, i)]);
-
+            allTiles[new Vector2Int(0, i)] = new PathTile(new Vector2Int(0, i), tileSize);
+            allTiles[new Vector2Int(0, i)].GeneratePath(Vector2Int.down, Vector2Int.up);
+            PaintPathTile(allTiles[new Vector2Int(0, i)]);
         }
-        
     }
     public void PaintPathTile(PathTile tile)
     {
@@ -58,9 +75,12 @@ public class PathGenerator : MonoBehaviour
             }
         }
     }
-    public void GenerateNextPath()
+    public void GenerateNextPaths()
     {
+        foreach (Path path in paths)
+        {
 
+        }
     }
 
 }
